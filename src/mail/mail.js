534b3afc6036b1f330ff8d6f27acc05d1f2a8fc1
mihai-mura <mihai.mura2@gmail.com>
2022-05-02 17:38:12 +0300

    password recovery email template

--- src/mail/mail.js
+++ src/mail/mail.js
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import path from 'path';
+import handlebars from 'nodemailer-express-handlebars';
 
 dotenv.config();
 
@@ -11,12 +13,35 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
+const handlebarsOptions = {
+	viewEngine: {
+		extName: '.handlebars',
+		partialsDir: path.resolve('./src/mail/templates'),
+		defaultLayout: false,
+	},
+	viewPath: path.resolve('./src/mail/templates'),
+	extName: '.handlebars',
+};
+
+transporter.use('compile', handlebars(handlebarsOptions));
+
 export const sendPassRecoverMail = async (to, link) => {
 	const mailOptions = {
 		from: process.env.EMAIL_USER,
 		to: to,
 		subject: 'Password Recovery',
-		text: `Click this link to recover your password: http://localhost:3000/recover-password/${link}`,
+		// text: `Click this link to recover your password: http://localhost:3000/recover-password/${link}`,
+		template: 'passRecovery',
+		context: {
+			link: `http://localhost:3000/recover-password/${link}`,
+		},
+		attachments: [
+			{
+				filename: 'logo-cityq.png',
+				path: path.resolve('./src/mail/images/logo-cityq.png'),
+				cid: 'logo',
+			},
+		],
 	};
 	await transporter
 		.sendMail(mailOptions)
